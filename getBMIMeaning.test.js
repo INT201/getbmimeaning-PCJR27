080dbdaa12afe94d662c5da7c99f69784858d1bf
porcha <porcha.por@mail.kmutt.ac.th>
2022-09-04 22:46:54 +0700

    update TestBMI

--- getBMIMeaning.test.js
+++ getBMIMeaning.test.js
@@ -1,14 +1,17 @@
- const testFn = require('./getBMIMeaning')
+ const getBMIMeaning = require('./getBMIMeaning')
+const testFn = require('./getBMIMeaning')
 
 test('output#1', () => {
   expect(testFn(65, 1.8)).toBe('Normal weight')
-  
+  console.log(getBMIMeaning(65,1.8))
 })
 
 test('output#2', () => {
   expect(testFn(80, 1.7)).toBe('Overweight')
+  console.log(getBMIMeaning(80,1.7))
 })
 
 test('output#3', () => {
   expect(testFn(44, 1.6)).toBe('Underweight')
+  console.log(getBMIMeaning(44,1.6))
 })
